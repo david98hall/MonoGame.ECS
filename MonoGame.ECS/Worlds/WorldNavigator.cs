23fbcc91522cf695c08e3ea446a1075b54ce101f
David Hall <welexay@gmail.com>
2019-11-16 02:12:35 +0100

    Create WorldNavigator.cs

---
+++ MonoGame.ECS/Worlds/WorldNavigator.cs
@@ -0,0 +1,159 @@
+﻿using MonoGame.Extended.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace MonoGame.ECS.Worlds
+{
+    /// <summary>
+    /// A singleton tool for navigating between worlds
+    /// </summary>
+    /// <typeparam name="T">E.g. an enum representing world identifications.</typeparam>
+    public class WorldNavigator<T> where T : struct, IConvertible
+    {
+
+        /// <summary>
+        /// The only instance of WorldNavigator.
+        /// </summary>
+        public static readonly WorldNavigator<T> INSTANCE = new WorldNavigator<T>();
+
+        #region Navigation history
+        /// <summary>
+        /// True if there is any back history.
+        /// </summary>
+        public bool CanNavigateBack => backHistory.Count > 0;
+
+        /// <summary>
+        /// True if there is any forward "history".
+        /// </summary>
+        public bool CanNavigateForward => forwardHistory.Count > 0;
+
+        /// <summary>
+        /// True if there has been at least one previous navigation.
+        /// </summary>
+        public bool HasNavigated => visitedWorlds.Count > 0;
+
+        private readonly Dictionary<T, World> visitedWorlds;
+
+        private readonly Stack<T> backHistory;
+
+        private readonly Stack<T> forwardHistory;
+        #endregion
+
+        #region World creation
+        public CreateWorld CreateWorldAction { get; set; }
+
+        public delegate World CreateWorld(T worldType);
+        #endregion
+
+        #region Current world
+        /// <summary>
+        /// The id of the current world (most recently navigated to).
+        /// </summary>
+        public T CurrentWorldId { get; private set; }
+
+        /// <summary>
+        /// The current world (most recently navigated to).
+        /// </summary>
+        public World CurrentWorld => visitedWorlds[CurrentWorldId];
+        #endregion
+
+        #region Events
+        public event NavigationHandler OnNavigation;
+        public event NavigationHandler OnBackNavigation;
+        public event NavigationHandler OnForwardNavigation;
+        public delegate void NavigationHandler(T worldId);
+        #endregion
+
+        private WorldNavigator()
+        {
+            visitedWorlds = new Dictionary<T, World>();
+            backHistory = new Stack<T>();
+            forwardHistory = new Stack<T>();
+        }
+
+        #region Navigation
+        public void NavigateTo(T worldId, bool tryFindOld = false)
+        {
+            // Manage navigation history
+            if (HasNavigated)
+            {
+                backHistory.Push(CurrentWorldId);
+            }
+            forwardHistory.Clear();
+
+            UpdateCurrentWorld(worldId, tryFindOld);
+        }
+
+        public void NavigateBack(bool tryFindOld = true)
+        {
+            forwardHistory.Push(CurrentWorldId);
+            UpdateCurrentWorld(backHistory.Pop(), tryFindOld);
+            OnBackNavigation?.Invoke(CurrentWorldId);
+        }
+
+        public void NavigateForward(bool tryFindOld = true)
+        {
+            backHistory.Push(CurrentWorldId);
+            UpdateCurrentWorld(forwardHistory.Pop(), tryFindOld);
+            OnForwardNavigation?.Invoke(CurrentWorldId);
+        }
+        #endregion
+
+        #region Clearing and resetting
+
+        /// <summary>
+        /// Clears all history.
+        /// </summary>
+        public void Reset()
+        {
+            visitedWorlds.Clear();
+            ClearHistory();
+        }
+
+        public void ClearHistory()
+        {
+            ClearBackHistory();
+            ClearForwardHistory();
+        }
+
+        public void ClearBackHistory() => backHistory.Clear();
+
+        public void ClearForwardHistory() => forwardHistory.Clear();
+        #endregion
+
+        #region Helper methods
+        private void UpdateCurrentWorld(T worldId, bool tryFindOld)
+        {
+            CreateWorldActionNullCheck();
+
+            // Get a world with the passed identification
+            var useOldWorld = tryFindOld && visitedWorlds.ContainsKey(worldId);
+            var world = useOldWorld
+                ? visitedWorlds[worldId]
+                : CreateWorldAction(worldId);
+
+            if (!useOldWorld)
+            {
+                // If chosen to create a new world, also add it to the 
+                // dictionary of all visited worlds
+                visitedWorlds.Add(worldId, world);
+            }
+
+            // Update current world
+            CurrentWorldId = worldId;
+
+            // Notify listeners
+            OnNavigation?.Invoke(CurrentWorldId);
+        }
+
+        private void CreateWorldActionNullCheck()
+        {
+            if (CreateWorldAction == null)
+            {
+                throw new Exception("The create world action (CreateWorldAction) is null!");
+            }
+        }
+        #endregion
+
+    }
+}
