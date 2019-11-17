340cc2eb5974283323f72b3d516ef9bd027d333e
David Hall <welexay@gmail.com>
2019-11-17 18:26:48 +0100

    Update MGStylizedText.cs

--- MonoGame.ECS/Components/Appearance/MGStylizedText.cs
+++ MonoGame.ECS/Components/Appearance/MGStylizedText.cs
@@ -16,7 +16,8 @@ namespace MonoGame.ECS.Components.Appearance
 
         public Vector2 RelativeCenterPosition { get; set; }
 
-        public MGStylizedText(string text, Color defaultColor, SpriteFont defaultFont) : base(text, defaultColor, defaultFont)
+        public MGStylizedText(string text, Color defaultColor, SpriteFont defaultFont, float maxWidth = float.MaxValue, float rowSpacing = 3)
+            : base(text, defaultColor, defaultFont, maxWidth, rowSpacing)
         {
             RelativeCenterPosition = Vector2.Zero;
         }
